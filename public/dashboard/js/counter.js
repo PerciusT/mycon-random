7b8aa67b8b6d470f054127e22eb9a808eff22580
Perccyus <shari200089@gmail.com>
2022-03-23 20:58:53 +0500

    heroku v9

--- public/dashboard/js/counter.js
+++ public/dashboard/js/counter.js
@@ -36,8 +36,12 @@ $(document).ready(function() {
 		$('#achsec').append(`<div class="righter" id="achitem`+ski+`">
                       <input class="app-form-control inputfile" type="file" id="achuser`+ski+`" name="achieve`+ski+`img" accept=".jpg,.jpeg,.png" placeholder="Achievement">
                       <label for="achuser`+ski+`" class="labelreceipt usersizer"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"></path></svg> <span>Custom `+ski+`</span></label>
+                      <img class="previewimg" id="achimg`+ski+`" src="#" alt="">
                       <br>
                     </div>`)
+		$("#achuser"+ski).change(function() {
+      		readURL(this);
+    	});
         
 	}
 	var i= $('#countsk').val()
@@ -91,9 +95,13 @@ $(document).ready(function() {
 		$('#achsec').append(`<div class="righter" id="achitem`+k+`">
                       <input class="app-form-control inputfile" type="file" id="achuser`+k+`" name="achieve`+k+`img" accept=".jpg,.jpeg,.png" placeholder="Achievement">
                       <label for="achuser`+k+`" class="labelreceipt usersizer"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"></path></svg> <span>Custom `+k+`</span></label>
+                      <img class="previewimg" id="achimg`+k+`" src="#" alt="">
                       <br>
                     </div>`)
         $('#countach').val(k)
+        $("#achuser"+k).change(function() {
+      		readURL(this);
+    	});
 	})
 	
 	$('#achminus').click(function(){
@@ -106,5 +114,22 @@ $(document).ready(function() {
 			$('#countxp').val(k)
 		}
 	})
+	function readURL(input) {
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        
+        reader.onload = function(e) {
+          console.log("#achimg"+input.id.slice(-1))
+          $("#achimg"+input.id.slice(-1)).attr('src', e.target.result);
+        }
+        
+        reader.readAsDataURL(input.files[0]); // convert to base64 string
+      }
+    }
+    
+    $("#achuser1").change(function() {
+      readURL(this);
+    });
+        
 
 })
